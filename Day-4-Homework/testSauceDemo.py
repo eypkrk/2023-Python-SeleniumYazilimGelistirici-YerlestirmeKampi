e110f6e523d8529a0dafeef97f16206c76103c5b
eypkrk <eyupkarakoyunlu@hotmail.com>
2023-05-02 22:50:07 +0300

    Ptyhon Eğitim Projesi

--- Day-4-Homework/testSauceDemo.py
+++ Day-4-Homework/testSauceDemo.py
@@ -11,7 +11,6 @@ class TestHomework:
         sleep(5)
         self.userName = self.driver.find_element(By.ID,"user-name")
         self.password = self.driver.find_element(By.ID,"password")
-        print(self.userName)
         sleep(2)
         self.loginBtn = self.driver.find_element(By.ID,"login-button")
         sleep(2)
@@ -21,9 +20,11 @@ class TestHomework:
         self.userName.send_keys("")
         sleep(2)
         self.loginBtn.click()
+        sleep(2)
         errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
         testResult = errorMessage.text == "Epic sadface: Username is required"
         print(f"Tüm Alan Boş Sonuç: {testResult}")
+
     def testLoginPasswordNull(self):
         self.userName.send_keys("eyüp")
         self.password.send_keys("")
@@ -33,18 +34,7 @@ class TestHomework:
         testResult = errorMessage.text == "Epic sadface: Password is required"
         print(f"Şifre Alanı Boş Sonuç: {testResult}")
         self.userName.clear()
-        
-    def testLoginLocked(self):
-        self.userName.send_keys("locked_out_user") 
-        self.password.send_keys("secret_sauce") 
-        sleep(2)
-        self.loginBtn.click()
-        errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
-        testResult = errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
-        print(f"Kilitli Kullanıcı Sonuç: {testResult}")
-        self.userName.clear()
-        self.password.clear()
-     
+
     def testLoginIcon(self):
         self.userName.send_keys("")
         self.password.send_keys("")
@@ -58,16 +48,30 @@ class TestHomework:
         errorBtn.click()
         for icon in errorIcon:
             print(f"Test X İcon Görünmüyor: {icon.is_displayed()}")
+        self.userName.clear()
+
+    def testLoginLocked(self):
+        self.userName.send_keys("locked_out_user") 
+        self.password.send_keys("secret_sauce") 
+        sleep(2)
+        self.loginBtn.click()
+        errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        testResult = errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
+        print(f"Kilitli Kullanıcı Sonuç: {testResult}")
+        self.userName.clear()
+
 
     def testLoginDirect(self):
         self.userName.send_keys("standard_user")
-        self.password.send_keys("secret_sauce")
         sleep(2)
         self.loginBtn.click()
+        sleep(2)
         getUrl = self.driver.current_url
         print(getUrl)
         testResult = getUrl == "https://www.saucedemo.com/inventory.html"
         print(f"Test Yönlendirilen Site: {testResult}")
+
+
     def testLenProduct(self):
         lenProduct = self.driver.find_elements(By.CLASS_NAME,"inventory_item")
         print(f"Ürün Sayısı: {len(lenProduct)}")
@@ -75,8 +79,8 @@ class TestHomework:
 testCheck = TestHomework()
 testCheck.testLoginAllNull()
 testCheck.testLoginPasswordNull()
-testCheck.testLoginLocked()
 testCheck.testLoginIcon()
+testCheck.testLoginLocked()
 testCheck.testLoginDirect()
 testCheck.testLenProduct()
 
